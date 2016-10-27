a7de99f7fb3ef0c76120ba244255fae81bc3d666
ar <a.rau@comlog.org>
2016-10-27 16:08:58 +0200

    ...

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('../');
 var fs = require('fs');
 
 try {
-    var result = request.get.sync('http://www.comlog.org/index.php');
+    var result = request.get.sync({uri:'http://www.comlog.org/index.php', formData: {test:'test'}});
     console.info(result);
 } catch (e) {
     console.error(e);
